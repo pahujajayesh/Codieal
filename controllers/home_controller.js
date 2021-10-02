cc229aecc2a0c98d6da2225c67e5881732d11252
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-10-03 03:19:57 +0530

    check authentication while adding posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,6 @@
 const Post=require('../models/post');
 
 module.exports.home=function(req,res){
-//    Post.find({},function(err,posts){
-//     return res.render('home',{
-//         title:"codieal Home",
-//         posts:posts
-//     });
-//    })
 
 Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home',{
