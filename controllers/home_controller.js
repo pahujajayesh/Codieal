764e9785a3c9b75ba941b42e59ad4b11561f114b
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-10-04 14:24:06 +0530

    add comments to post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,11 +2,18 @@ const Post=require('../models/post');
 
 module.exports.home=function(req,res){
 
-Post.find({}).populate('user').exec(function(err,posts){
-    return res.render('home',{
-        title:"codieal Home",
-        posts:posts
-});
-   
-});
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
 }
