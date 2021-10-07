4ac161dc79d276badcb60143220b1acb9ae8a921
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-10-07 12:38:20 +0530

    add feature delete post

--- routes/post.js
+++ routes/post.js
@@ -5,4 +5,7 @@ const router=express.Router();
 const postsController=require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication,postsController.create);
+
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
+
 module.exports=router;
