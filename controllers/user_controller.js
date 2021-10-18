7678edcdad5d76d0b423bae658fa2ac1b765b5e8
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-10-18 23:04:41 +0530

    handle edge case in avatar

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
-
+const fs=require('fs');
+const path=require('path');
 module.exports.profile = function(req, res){
     User.findById(req.params.id,function(err,user){
       return res.render('user_profile',{
@@ -20,7 +21,11 @@ module.exports.update = async function(req, res){
         }
         user.name=req.body.name;
         user.email=req.body.email;
+
         if(req.file){
+          if(user.avatar){
+            fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+          }
           user.avatar=User.avatarPath+'/'+req.file.filename;
         }
         user.save();
